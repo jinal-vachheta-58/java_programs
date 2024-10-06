f05390ce09c00f323d80a13c0fab14741fa445ab
jinal vachheta <jinalvachheta@gmail.com>
2024-10-07 02:51:24 +0530

    check num is palindrom or not

--- java_practice_library/assignment-1/palindrome_num.java
+++ java_practice_library/assignment-1/palindrome_num.java
@@ -4,8 +4,25 @@ palindrome or not. */
  * palindrome_num
  */
 public class palindrome_num {
-
     public static void main(String[] args) {
-
+        int num = 12321;
+        int rev_num = 0;
+        int ori_num = num;
+        while(num != 0 )
+        {
+            int digit = num % 10;
+            rev_num = (rev_num * 10) + digit;
+            num = num / 10;
+        }
+        if (ori_num == rev_num) {
+            System.out.println(ori_num + " == " + rev_num);
+        }else{
+            System.out.println(ori_num + " != " + rev_num);
+        }
     }
 }
+
+/*output :
+123 != 321
+12321 == 12321
+ */
