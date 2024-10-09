f0843696e0ea2c4b6d369f6032a1e420452f8995
jinal vachheta <jinalvachheta@gmail.com>
2024-10-09 20:08:51 +0530

    find length of array

---
+++ java_practice_library/assignment-2/array_length_1.java
@@ -0,0 +1,11 @@
+// package assignment-2;
+public class array_length_1 {
+    public static void main(String[] args) {
+        int a[] = new int[10];
+        int t = 0;
+        for (int i = 0; i < a.length; i++) {
+            t++;
+        }
+        System.out.println(t);
+    }
+}
