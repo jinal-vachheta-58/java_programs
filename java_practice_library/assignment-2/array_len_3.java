cc9f88053f5244755f336516902558a2c8c85fbf
jinal vachheta <jinalvachheta@gmail.com>
2024-10-09 20:20:10 +0530

    find len using function

---
+++ java_practice_library/assignment-2/array_len_3.java
@@ -0,0 +1,10 @@
+class array_len_3 {
+
+    public static void main(String[] args) {
+       int[] var1 = new int[10];
+       int var2 = var1.length;
+       System.out.println("Length of the array: " + var2);
+      //  var1= new int[11]; 
+    }
+ }
+ 
