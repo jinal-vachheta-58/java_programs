72a4cc387b6943ecbe50e557da1337a3c16ed360
Jinal P. Vachheta <156422920+jinal-vachheta-58@users.noreply.github.com>
2025-01-09 17:50:26 +0530

    basic string operation

---
+++ java_practice_library/assignment-2/basic_str.java
@@ -0,0 +1,9 @@
+class basic_str{
+    public static void main(String[] args)
+    {
+        String str = new String("xyz");
+        System.out.println(str);
+        str =  "jinal";
+        System.out.println(str);
+    }
+}
