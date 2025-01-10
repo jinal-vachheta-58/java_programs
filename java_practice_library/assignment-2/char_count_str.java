590d6599ee7324f9967d7500544dbfff246783f6
Jinal P. Vachheta <156422920+jinal-vachheta-58@users.noreply.github.com>
2025-01-10 10:45:13 +0530

    character count in string

--- java_practice_library/assignment-2/char_count_str.java
+++ java_practice_library/assignment-2/char_count_str.java
@@ -1,17 +1,22 @@
-// Write and run a JAVA program that reads a string from the user and perform the following.
-//  counts number of occurance of a given character (for example, " a") in a string.
 
+/*Write and run a JAVA program that reads a string from the user and perform the following.
+ counts number of occurance of a given character (for example, " a") in a string.
+
+searches the last occurance of a character in a string.
+ */
 import java.util.Scanner;
 
-public class char_count_str {
+class char_count_str {
     public static void main(String[] args) {
-
         Scanner input = new Scanner(System.in);
         System.out.println("Enter a string : ");
         String str = input.nextLine();
         char_count_str ccs = new char_count_str();
         int c = ccs.count_char_occur(str, 'a');
         System.out.println("count_char_occur : " + c);
+        
+        int last_occurence = ccs.last_occurence(str, 'a');
+        System.out.println("last_occurence : " + last_occurence);
 
         input.close();
     }
@@ -25,8 +30,15 @@ public class char_count_str {
         }
         return count;
     }
-}
 
-// output:
-// jinal vachheta
-// count_char_occur : 3
+    int last_occurence(String str,char c)
+    {
+        int index = -1;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == c) {
+                index = i;   
+            }
+        }
+        return index;
+    }
+}
