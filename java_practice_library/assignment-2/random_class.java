db8bc1dff6824a2d6f44556be284b5960db24788
Jinal P. Vachheta <156422920+jinal-vachheta-58@users.noreply.github.com>
2025-01-10 11:37:08 +0530

    random class and random method from math class

---
+++ java_practice_library/assignment-2/random_class.java
@@ -0,0 +1,42 @@
+package a_sesional_1_last_practice;
+
+// import java.util.*;
+// import java.lang.Math.random;
+
+class randam {
+    public static void main(String[] args) {
+        int x = (int)(Math.random() * 10);
+        System.out.println(x);
+    }    
+}
+
+
+// package a_sesional_1_last_practice;
+
+// // package a_sesional_1_last_practice;
+// import java.util.*;
+
+// import javax.swing.plaf.TreeUI;
+
+// import java.lang.Math.*;
+
+
+// class randam {
+//     public static void main(String[] args) {
+//         // Random rand = new Random();
+//         // int i = rand.nextInt(2);
+//         // System.out.println(i);
+
+//         // double rand_dub1 = rand.nextDouble();
+//         // double rand_dub2 = rand.nextDouble();
+   
+//         // // Print random doubles
+//         // System.out.println("Random Doubles: "+rand_dub1);
+//         // System.out.println("Random Doubles: "+rand_dub2);
+
+//         int x = (int)(Math.random() * 10);
+//         System.out.println(x);
+        
+        
+//     }    
+// }
