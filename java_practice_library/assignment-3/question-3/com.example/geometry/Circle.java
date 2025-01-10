679d104f8761011d466e749eae030cb26f44e8b2
Jinal P. Vachheta <156422920+jinal-vachheta-58@users.noreply.github.com>
2025-01-10 11:53:14 +0530

    que-3 complete

---
+++ java_practice_library/assignment-3/question-3/com.example/geometry/Circle.java
@@ -0,0 +1,32 @@
+package com.example.geometry;
+
+public class Circle {
+    private double r;
+
+    // Constructor to initialize radius
+    public Circle(double radius) {
+        this.r = radius;
+    }
+
+    // Method to calculate the area of a circle
+    public double area() {
+        
+        return 3.14 * r * r;
+    }
+}
+
+ class Rectangle {
+    private double l;
+    private double b;
+
+    // Constructor to initialize length and breadth
+    public Rectangle(double len, double br) {
+        this.l = len;
+        this.b = br;
+    }
+
+    // Method to calculate the area of a rectangle
+    public double area() {
+        return l * b;
+    }
+}
